4f5c5d506d917099357e850fe62eeda2931ab04e
DenSTR <web_life@inbox.ru>
2023-04-14 10:23:11 +0300

    добавялет стили

--- src/components/pages/Index/TopSales/ItemView/index.jsx
+++ src/components/pages/Index/TopSales/ItemView/index.jsx
@@ -1,7 +1,16 @@
+import styles from "./item.module.scss";
+
 export default function ItemView({item}) {
+
   return (
-    <li>
-      <div>sdfsdf</div>
+    <li className={styles['item-comp']}>
+      <div className="card">
+        <img src={item.images[0]} className="card-img-top" alt="item.title" />
+          <div className="card-body">
+            <h2>{item.title}</h2>
+            <div className="card-text">{item.price}</div>
+          </div>
+      </div>
     </li>
   )
 }
--- src/components/pages/Index/TopSales/index.jsx
+++ src/components/pages/Index/TopSales/index.jsx
@@ -1,12 +1,13 @@
 import ItemView from "@/components/pages/Index/TopSales/ItemView";
 import {useSelector} from "react-redux";
 import {hitsList} from "@/redux/hits";
+import styles from "./salesList.module.scss";
 
 export default function SalesList() {
   const hits = useSelector(hitsList) || [];
   const listView = hits.map(item => <ItemView key={item.id} item={item} />)
   return (
-    <ul className="sales-list-copm">
+    <ul className={styles['sales-list-comp']}>
       {listView}
     </ul>
   )
