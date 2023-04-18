3238860b4dec99f5842889495a47da785e15c28f
DenSTR <web_life@inbox.ru>
2023-04-18 18:21:51 +0300

    добавляет активный класс для меню

--- src/components/general/AppHeader/index.jsx
+++ src/components/general/AppHeader/index.jsx
@@ -1,9 +1,21 @@
 import Link from "next/link";
 import styles from "./header.module.scss";
 import {useState} from "react";
+import {useRouter} from "next/router";
+import navMenu from "@/mock/navMenu.json";
+
 
 export default function AppHeader() {
   let [showSearch, setShowSearch] = useState(false);
+  const router = useRouter();
+  const currentRoute = router.pathname;
+  const navLinks = navMenu.map(({path, name}) =>
+    <li
+      className="nav-item"
+      key={path}
+    >
+      <Link href={path} className={`${currentRoute === path ? 'nav-link active' : 'nav-link'}`}>{name}</Link>
+    </li>)
 
   return (
     <header className="container">
@@ -11,29 +23,20 @@ export default function AppHeader() {
         <div className="col">
           <nav className="navbar navbar-expand-sm navbar-light bg-light">
             <Link className="navbar-brand" href="/">
-              <img src="/img/header-logo.png" alt="Bosa Noga" />
+              <img src="/img/header-logo.png" alt="Bosa Noga"/>
             </Link>
-            <div className="collapase navbar-collapse" id="navbarMain">
+            <div className="collapase navbar-collapse justify-content-between" id="navbarMain">
               <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" href="/">Главная</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" href="/catalog">Каталог</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" href="/about">О магазине</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" href="/contacts">Контакты</Link>
-                </li>
+                {navLinks}
               </ul>
               <div>
                 <div className={styles.pics}>
-                  <form data-id="search-form" className={`${styles['search-form']} form-inline ${showSearch ? '' : 'invisible'}`.trim()}>
-                    <input className={`${styles['form-control']}`} placeholder="Поиск" />
+                  <form data-id="search-form"
+                        className={`${styles['search-form']} form-inline ${showSearch ? '' : 'invisible'}`.trim()}>
+                    <input className={`${styles['form-control']}`} placeholder="Поиск"/>
                   </form>
-                  <div className={`${styles.pic} ${styles.search}`} onClick={() => setShowSearch(showSearch = !showSearch)} />
+                  <div className={`${styles.pic} ${styles.search}`}
+                       onClick={() => setShowSearch(showSearch = !showSearch)}/>
                   <Link className={`${styles.pic} ${styles.cart}`} href="/cart">
                     <div className={styles['cart-full']}>1</div>
                     <div className="header-controls-cart-menu"></div>
